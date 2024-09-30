a09ce65e21ce180048ab01c3868642c62c22577b
Thorsten Kurth <tkurth@nvidia.com>
2024-09-30 06:52:53 -0700

    fixing more issues

--- torch/csrc/api/include/torch/optim/optimizer.h
+++ torch/csrc/api/include/torch/optim/optimizer.h
@@ -41,7 +41,7 @@ void _device_dtype_check_for_fused(const Tensor& param, bool cuda_unsupported = 
   bool supported = torch::is_floating_point(param);
   auto pdevice = c10::DeviceTypeName(param.device().type(), true);
   auto has_device = [&pdevice](std::string dev) { return dev == pdevice; };
-  supported |= std::find_if(supported_devices.begin(), supported_devices.end(), has_device);
+  supported |= (std::find_if(supported_devices.begin(), supported_devices.end(), has_device) != supported_devices.end());
 
   TORCH_CHECK(!supported, "`fused=True` requires all the params to be floating point Tensors of supported devices");
 }
